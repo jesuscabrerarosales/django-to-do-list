b32a6be0e312bb465f0a0a710e767a012eb95482
Jesús E. Cabrera Rosales <jesuscabrerarosales47@gmail.com>
2025-04-09 16:53:00 -0500

    create redirecction by the type of request.method

--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,19 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-def hello_world(request):
-    return render(request, 'signup.html', {''
-    'form':UserCreationForm
-    })
+# def hello_world(request):
+#     return render(request, 'signup.html', {''
+#     'form':UserCreationForm
+#     })
+
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if(request.method == 'GET'):
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        return render(request, 'home.html')
+    
