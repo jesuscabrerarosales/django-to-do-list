4a55426cbe23fc1aaaf502c864ed933241479337
Jesús E. Cabrera Rosales <jesuscabrerarosales47@gmail.com>
2025-04-09 14:41:23 -0500

    create basic routes and signup form

---
+++ tasks/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+def hello_world(request):
+    return render(request, 'signup.html', {''
+    'form':UserCreationForm
+    })
