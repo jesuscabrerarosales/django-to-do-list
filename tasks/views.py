ed5b468db5b614cc9942c94229a3527fe6fb17b7
Jesús E. Cabrera Rosales <jesuscabrerarosales47@gmail.com>
2025-04-11 16:48:44 -0500

    add validations and cookie of login

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 # Create your views here.
 # def hello_world(request):
 #     return render(request, 'signup.html', {''
@@ -17,5 +18,22 @@ def signup(request):
             'form': UserCreationForm
         })
     else:
-        return render(request, 'home.html')
-    
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+                                     password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+               return render(request, 'signup.html', {
+                'form': UserCreationForm,
+                "error": 'Username already exists'
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            "error": 'Password do not match'
+        })
+ 
+def tasks(request):
+    return render(request, 'tasks.html')   
